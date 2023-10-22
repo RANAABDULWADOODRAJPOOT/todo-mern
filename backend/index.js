ed42dc6ea2f1c3bdff337e66a75a8b6b36c395a4
Abdul Wadood <49836993+RANAABDULWADOODRAJPOOT@users.noreply.github.com>
2023-10-22 14:06:00 +0500

    Update Frontend

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const app=express();
+const cors=require('cors');
 const { connectToDatabase } = require('./db');
 
 
 connectToDatabase();
 
 app.use(express.json());
+app.use(cors());
 
 
 app.use('/api', require('./api/index'));
--- frontend/src/app/todo/edit/[id]/page.js
+++ frontend/src/app/todo/edit/[id]/page.js
@@ -1,35 +1,46 @@
 'use client';
 import { Button, Input, message } from "antd";
 import axios from "axios";
-import { useRouter, useParams } from "next/navigation";
+import { useRouter, useParams, usePathname } from "next/navigation";
 import { useState, useEffect } from "react";
 
 export default function Page() {
     const [title, setTitle] = useState();
     const [description, setDescription] = useState();
     const router = useRouter();
-
+    const routerParams=useParams();
+    const {id}=routerParams;
 
     useEffect(() => {
-
+        getTodo(id);
         return () => {
             setDescription();
             setTitle();
         }
     }, []);
 
+    const getTodo=async(id)=>{
+        await axios.get(`http://localhost:3000/api/todo/${id}`).then((response)=>{
+           setTitle(response?.data?.title);
+           setDescription(response?.data?.description);
+         }).catch((err)=>{
+           console.log(err, "Error");
+         })
+       }
+       
+
     const submit = async (e) => {
         if (!description || !title) {
             message.error("Title and Description are Requried");
             return;
         }
-        const response = await axios.post('http://localhost:3000/api', {
+        const response = await axios.put(`http://localhost:3000/api/${id}`, {
             title,
             description
         });
 
         if (response?.data?.title) {
-            message.info("Todo Created Successfully");
+            message.info("Todo Updated Successfully");
             router.push('/');
         } else {
             message.error("Creation of Todo is Unsuccessful");
